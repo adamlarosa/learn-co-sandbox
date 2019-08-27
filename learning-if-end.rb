cb61a2acebb75ad066dba00038ffa645af87de54
Adam La Rosa <adam.larosa@gmail.com>
2019-08-27 23:22:36 +0000

    Done.

--- learning-if-end.rb
+++ learning-if-end.rb
@@ -1,6 +1,14 @@
-chance_of_rain = 0.2
-if (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "Pack and umbrella!"
-else
-  puts "Stay home and read Hegel."
+#chance_of_rain = -23
+#if (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+#  puts "Pack and umbrella!"
+#else
+#  puts "Stay home and read Hegel."
+#end
+
+greeting = "friendly_greeting"
+case greeting
+  when "unfriendly_greeting"
+    puts "What do you want!?"
+  when "friendly_greeting"
+    puts "Hi!  How are you?"
 end
