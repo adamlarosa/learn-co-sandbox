2f2a6fc3c56a8dd235ddc4c378cc9f693d82b6fb
Adam La Rosa <adam.larosa@gmail.com>
2019-08-28 20:11:12 +0000

    Done.

--- learning-if-end.rb
+++ learning-if-end.rb
@@ -6,9 +6,3 @@
 #end
 
 greeting = "friendly_greeting"
-case greeting
-  when "unfriendly_greeting"
-    puts "What do you want!?"
-  when "friendly_greeting"
-    puts "Hi!  How are you?"
-end
---
+++ while-do.rb
@@ -0,0 +1,6 @@
+count = 0
+
+while count < 3 do
+  puts "I am the #{count}, I love to count"
+  count += 1
+end
