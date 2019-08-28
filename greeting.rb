0fb5043d00e142caf56550e4d1cd62bb63e46991
Adam La Rosa <adam.larosa@gmail.com>
2019-08-28 21:07:33 +0000

    Done.

--- greeting.rb
+++ greeting.rb
@@ -1,3 +1,3 @@
-def greeting_a_person(name)
-  puts "Hello #{name}"
+def greeting_programmer(name, language="computer")
+  puts "Hello, #{name}.  We heard you are a great #{language} programmer."
 end
